d9b6b154f4064422ab7931865aeff29f39d3386a
AR3 <ar3@ar3.me>
2014-08-13 20:03:43 -0400

    Create a PigLatin class that is initialized with a string

--- lib/piglatin.rb
+++ lib/piglatin.rb
@@ -1,2 +1,6 @@
 class Piglatin
+  attr_accessor :phrase
+  def initialize the_phrase
+    self.phrase = the_phrase
+  end
 end
--- spec/piglatin_spec.rb
+++ spec/piglatin_spec.rb
@@ -1,11 +1,22 @@
 require 'piglatin'
 
-describe Piglatin do
-  describe "#initialize" do
-    it "instantiates" do
-      expect {
-        Piglatin.new
-      }.to_not raise_error
-    end
+describe Piglatin, '#phrase' do
+  it "should The string is a list of words seperated by spaces: 'hello world'" do
+    expect {
+      translator = Piglatin.new 'hello world'
+    }.to_not raise_error
+  end
+
+  it 'should The string is accessed by a method named phrase' do
+    translator = Piglatin.new 'something'
+    expect(translator.phrase).to eq 'something'
+  end
+
+  it 'should The string can be reset at any time without re-initializing' do
+    translator = Piglatin.new 'something'
+    translator.phrase = 'something else'
+    expect(translator.phrase).to eq 'something else'
   end
 end
+
+
